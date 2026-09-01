72a5607bc3c89fe790735c659a6fe116b7a154b2
오세영 <leenight5309@naver.com>
2026-09-01 19:54:58 +0900

    feat: 0901 수업 끝

--- board/src/main/java/com/mirim/board/PostController.java
+++ board/src/main/java/com/mirim/board/PostController.java
@@ -1,8 +1,14 @@
 package com.mirim.board;
 
+import ch.qos.logback.core.joran.action.ResourceAction;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.ResourceBundle;
 
 @RestController
 @RequestMapping("/posts")
@@ -27,8 +33,21 @@ public class PostController {
     }
 
     @GetMapping("/{id}")
-    public String getPost(@PathVariable Long id) {
-        return id + "번 게시글입니다";
+    public ResponseEntity<?> getPost(@PathVariable Long id) {
+        // 게시글 번호가 10번보다 크면 게시글이 존재 하지 않는다.
+        if (id > 10) {
+            // 404
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("존재하지 않는 게시글입니다.");
+//            ResponseEntity.notFound();
+        } else if (id <= 0){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("잘못된 요청입니다.");
+        }else {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(id + "번 게시글입니다.");
+        }
+//        return id + "번 게시글입니다";
     }
 
     @GetMapping("/count")
@@ -37,13 +56,20 @@ public class PostController {
     }
 
     @PostMapping
-    public String createPost(@RequestBody Map<String, Object> request) {
+    public ResponseEntity<?> createPost(@RequestBody Map<String, Object> request) {
         String title = (String) request.get("title");
         String content = (String) request.get("content");
         //Long user_id = (Long) request.get("user_id");
 
         // DB 로직
+        Map<String, Object> response = new HashMap<>();
+        response.put("title", title);
+        response.put("content", content);
+        response.put("message", "게시글이 등록되었습니다.");
 
-        return "[" + title + "] 게시글이 등록되었습니다. 내용 : " + content;
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(response);
+
+        //return "[" + title + "] 게시글이 등록되었습니다. 내용 : " + content;
     }
 }
