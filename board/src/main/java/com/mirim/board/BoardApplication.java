f8aa8e9336dded016965ffe07e01bee247aaf638
오세영 <leenight5309@naver.com>
2026-09-08 19:51:47 +0900

    feat: 0908 수업 끝 / Bean, 의존성, IOC, DI, 생성자 주입 개념 공부

--- board/src/main/java/com/mirim/board/BoardApplication.java
+++ board/src/main/java/com/mirim/board/BoardApplication.java
@@ -1,7 +1,9 @@
 package com.mirim.board;
 
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BoardApplication {
@@ -13,7 +15,20 @@ public class BoardApplication {
         // 3. 내장 톰캣(서버)을 띄운다. 그에 맞는 포트(8080)가 열린다.
         // 4. 요청을 받아서 요청이 오면 알맞는 코드로 넘겨준다.
 
-        SpringApplication.run(BoardApplication.class, args);
+        ApplicationContext context = SpringApplication.run(BoardApplication.class, args);
+        Notifier notifier = context.getBean(Notifier.class);
+        notifier.send("컨테이너에서 직접 꺼낸 테스트 메시지입니다.");
+
+        try {
+            SmsNotifier smsNotifier = context.getBean(SmsNotifier.class);
+            smsNotifier.send("컨테이너에서 직접 꺼낸 테스트 메시지입니다.");
+        } catch (NoSuchBeanDefinitionException e) {
+            System.out.println("SmsNOtifier는 컨테이너에 없음: "+ e.getMessage());
+        }
+
+        System.out.println("등록된 Bean 개수: " + context.getBeanDefinitionNames().length);
+        System.out.println("emailNotifier 등록 여부: " + context.containsBean("emailNotifier"));
+        System.out.println("smsNotifier 등록 여부: " + context.containsBean("smsNotifier"));
     }
 
 }
